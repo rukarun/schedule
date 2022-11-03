bd6fa68b80a062c66056f095edb80e037a544f70
Ubuntu <ubuntu@ip-172-31-3-182.us-east-2.compute.internal>
2022-11-03 11:46:46 +0000

    メッセージ

--- myapp/app/controllers/posts_controller.rb
+++ myapp/app/controllers/posts_controller.rb
@@ -20,4 +20,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:name, :start, :end, :all_day, :introduction))
+      flash[:notice] = "ユーザーIDが「#{@post.id}」の情報を更新しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :posts
+  end
 end
+
--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_25_034536) do
+ActiveRecord::Schema.define(version: 2022_10_28_153026) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
